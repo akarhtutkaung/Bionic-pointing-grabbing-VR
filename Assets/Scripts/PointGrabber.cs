f106e66ced9e18a61f3280679e9a0d2c114435e0
Ace <akarthut99@gmail.com>
2021-12-01 18:52:14 -0600

    work-in progress

---
+++ Assets/Scripts/PointGrabber.cs
@@ -0,0 +1,138 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PointGrabber : Grabber
+{
+    public LineRenderer laserPointer;
+    public Material grabbablePointerMaterial;
+
+    public InputActionProperty touchAction;
+    public InputActionProperty grabAction;
+
+    public InputActionProperty moveAction;
+
+    Material lineRendererMaterial;
+    Transform grabPoint;
+    Grabbable grabbedObject;
+    Transform initialParent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        laserPointer.enabled = false;
+        lineRendererMaterial = laserPointer.material;
+
+        grabPoint = new GameObject().transform;
+        grabPoint.name = "Grab Point";
+        grabPoint.parent = this.transform;
+        grabbedObject = null;
+        initialParent = null;
+
+        grabAction.action.performed += Grab;
+        grabAction.action.canceled += Release;
+
+        touchAction.action.performed += TouchDown;
+        touchAction.action.canceled += TouchUp;
+
+        moveAction.action.performed += move;
+    }
+
+    private void OnDestroy()
+    {
+        grabAction.action.performed -= Grab;
+        grabAction.action.canceled -= Release;
+
+        touchAction.action.performed -= TouchDown;
+        touchAction.action.canceled -= TouchUp;
+
+        moveAction.action.performed -= move;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (laserPointer.enabled)
+        {
+            RaycastHit hit;
+            if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, Mathf.Infinity))
+            {
+                laserPointer.SetPosition(1, new Vector3(0, 0, hit.distance));
+
+                if (hit.collider.GetComponent<Grabbable>())
+                {
+                    laserPointer.material = grabbablePointerMaterial;
+                }
+                else
+                {
+                    laserPointer.material = lineRendererMaterial;
+                }
+            }
+            else
+            {
+                laserPointer.SetPosition(1, new Vector3(0, 0, 100));
+                laserPointer.material = lineRendererMaterial;
+            }
+        }
+    }
+
+    public override void Grab(InputAction.CallbackContext context)
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, Mathf.Infinity))
+        {
+            if (hit.collider.GetComponent<Grabbable>())
+            {
+                grabPoint.localPosition = new Vector3(0, 0, hit.distance);
+
+                if (hit.collider.GetComponent<Grabbable>().GetCurrentGrabber() != null)
+                {
+                    hit.collider.GetComponent<Grabbable>().GetCurrentGrabber().Release(new InputAction.CallbackContext());
+                }
+
+                grabbedObject = hit.collider.GetComponent<Grabbable>();
+                grabbedObject.SetCurrentGrabber(this);
+
+                if (grabbedObject.GetComponent<Rigidbody>())
+                {
+                    grabbedObject.GetComponent<Rigidbody>().isKinematic = true;
+                    grabbedObject.GetComponent<Rigidbody>().useGravity = false;
+                }
+
+                initialParent = grabbedObject.transform.parent;
+                grabbedObject.transform.parent = grabPoint;
+            }
+        }
+    }
+
+    public override void Release(InputAction.CallbackContext context)
+    {
+        if (grabbedObject)
+        {
+            if (grabbedObject.GetComponent<Rigidbody>())
+            {
+                grabbedObject.GetComponent<Rigidbody>().isKinematic = false;
+                grabbedObject.GetComponent<Rigidbody>().useGravity = true;
+            }
+
+            grabbedObject.transform.parent = initialParent;
+            grabbedObject = null;
+        }
+    }
+
+    void TouchDown(InputAction.CallbackContext context)
+    {
+        laserPointer.enabled = true;
+    }
+
+    void TouchUp(InputAction.CallbackContext context)
+    {
+        laserPointer.enabled = false;
+    }
+
+    public void move(InputAction.CallbackContext context){
+        if(grabbedObject){
+            Vector2 vec = context.ReadValue<Vector2>();
+            grabPoint.localPosition += new Vector3(0, 0, vec.y*0.05f);
+        }
+    }
+}
---
+++ Assets/Scripts/TelekinesisGrabber.cs
@@ -0,0 +1,113 @@
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class TelekinesisGrabber : Grabber
+{
+    public LineRenderer laserPointer;
+    public Material grabbablePointerMaterial;
+    public InputActionProperty touchAction;
+    public InputActionProperty selectAction;
+    public InputActionProperty grabAction;
+
+    Material lineRendererMaterial;
+    // Start is called before the first frame update
+    void Start()
+    {
+        laserPointer.enabled = false;
+        lineRendererMaterial = laserPointer.material;
+
+        selectAction.action.performed += Select;
+        selectAction.action.canceled += selectRelease;
+
+        touchAction.action.performed += TouchDown;
+        touchAction.action.canceled += TouchUp;
+
+        grabAction.action.performed += Grab;
+        grabAction.action.canceled += Release;
+    }
+
+    private void OnDestroy()
+    {
+        selectAction.action.performed -= Select;
+        selectAction.action.canceled -= selectRelease;
+
+        touchAction.action.performed -= TouchDown;
+        touchAction.action.canceled -= TouchUp;
+
+        grabAction.action.performed -= Grab;
+        grabAction.action.canceled -= Release;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (laserPointer.enabled)
+        {
+            RaycastHit hit;
+            if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, Mathf.Infinity))
+            {
+                laserPointer.SetPosition(1, new Vector3(0, 0, hit.distance));
+
+                if (hit.collider.GetComponent<Grabbable>())
+                {
+                    laserPointer.material = grabbablePointerMaterial;
+                }
+                else
+                {
+                    laserPointer.material = lineRendererMaterial;
+                }
+            }
+            else
+            {
+                laserPointer.SetPosition(1, new Vector3(0, 0, 40));
+                laserPointer.material = lineRendererMaterial;
+            }
+        }
+    }
+
+    void Select(InputAction.CallbackContext context)
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, Mathf.Infinity))
+        {
+            if (hit.collider.GetComponent<Grabbable>())
+            {
+                // add the object to the select list
+                // first get the velocity > x
+                // second get the velocity reducing or to 0
+                // then make the object come toward the eye
+                // activate the floating stance of the object
+                // disable the gravity of the object
+                // enable the kinematic of the object
+            }
+        }
+    }
+
+    void selectRelease(InputAction.CallbackContext context)
+    {
+        // enable the gravity of the object
+        // disable the kinematic of the object
+    }
+
+    void TouchDown(InputAction.CallbackContext context)
+    {
+        laserPointer.enabled = true;
+    }
+
+    void TouchUp(InputAction.CallbackContext context)
+    {
+        laserPointer.enabled = false;
+    }
+
+    public override void Grab(InputAction.CallbackContext context){
+        // grab the item
+    }
+
+    public override void Release(InputAction.CallbackContext context){
+        // release the item
+    }
+}
